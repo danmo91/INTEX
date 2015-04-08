d829927bfb2d6ff26d2008d5aced5b09c6b7b021
danmo91 <dan.morain91@gmail.com>
2015-04-08 15:37:31 -0600

    bd

--- manager/views/return.py
+++ manager/views/return.py
@@ -1,7 +1,6 @@
 from django_mako_plus.controller.router import get_renderer
 from django_mako_plus.controller import view_function
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from datetimewidget.widgets import DateWidget
 from django.core.mail import send_mail, EmailMessage
 import datetime
 from django import forms
