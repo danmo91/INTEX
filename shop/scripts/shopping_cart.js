40686a85ecd9adba9fef1b7f88eb502cf586255e
danmo91 <dan.morain91@gmail.com>
2015-04-08 16:16:00 -0600

    bd

--- shop/scripts/shopping_cart.js
+++ shop/scripts/shopping_cart.js
@@ -25,6 +25,8 @@ $(function() {
       onShow: function(dlg) {
         $('.button-panel').find('.btn').addClass('pull-right btn-warning')
         $('.button-panel').css({height: "60px"})
+        $('.modal-content').prop('data-backdrop','false')
+        $('.modal-content').prop('z-index','1020')
       },
 
     }); // loadmodal
